8d157d31ce9c2eceb1f4a74b527a01207e36a315
root <root@F005Poste10.btsinfogap.org>
2013-08-09 16:42:30 +0200

    Modification formVISITEUR.php

--- formVISITEUR.php
+++ formVISITEUR.php
@@ -10,42 +10,99 @@ if ( ! estVisiteurConnecte() )
 
 include("./scripts/entete.html");
 include("./scripts/menuGauche.html");
+if(isset($_POST['btActionFormChoixDep']))
+{//début if
+		$_SESSION['departement']=$_POST['lstDept'];	
+}//fin if
+
+if(isset($_POST['btActionFormChoixVisiteur']))
+{//début if
+		$_SESSION['visiteur']=$_POST['lstVisiteur'];	
+}//fin if
 ?>
 <div id="contenu">
-	<form name="formVISITEUR" method="post" action="">
+	<form name="formChoixDep" method="post" action="">
 		<h1> Visiteurs </h1>
 		<?
 		// récupération des département
-		// il faut faire la table DEPARTEMENT
-		$req="Select DEP_CODE,DEP_NOM from VISITEUR natural join DEPARTEMENT order by DEP_NOM";
-		echo $req;
+		$req="select distinct DEP_CODE, DEP_NOM from VISITEUR natural join DEPARTEMENT order by DEP_NOM;";
 		$resultat=mysql_query($req);
 		?>
 		<select name="lstDept" class="titre">
 		<?
 		while($ligne=mysql_fetch_array($resultat))
-		{
-		$nomDep=$ligne['DEP_NOM'];
-		$codeDep=$ligne['DEP_CODE'];
-		?>
-		<option value="<?=$codeDep?>"><?=$nomDep?></option>
+		{//début while
+			$nomDep=$ligne['DEP_NOM'];
+			$codeDep=$ligne['DEP_CODE'];
+			?>
+			<option value="<?=$codeDep?>"
+			<?
+			if($_SESSION['departement']==$codeDep)
+			{//début if
+				echo "selected";
+			}//fin if
+			?>
+			><?=$nomDep?></option>
 		<?
-		}
+		}//fin while
 		?>
 		</select>
-		<input type="submit" name="Selectionner" value="Selectionner"/>
+		<input type="submit" name="btActionFormChoixDep" value="Selectionner"/>
 		</form>
-		<form action="" method="POST">
-		<select name="lstVisiteur" class="zone"><option value=""></option></select>
-		<label class="titre">NOM :</label><input type="text" size="25" name="VIS_NOM" class="zone" />
-		<label class="titre">PRENOM :</label><input type="text" size="50" name="Vis_PRENOM" class="zone" />
-		<label class="titre">ADRESSE :</label><input type="text" size="50" name="VIS_ADRESSE" class="zone" />
-		<label class="titre">CP :</label><input type="text" size="5" name="VIS_CP" class="zone" />
-		<label class="titre">VILLE :</label><input type="text" size="30" name="VIS_VILLE" class="zone" />
-		<label class="titre">SECTEUR :</label><input type="text" size="1" name="SEC_CODE" class="zone" />
-		<label class="titre">&nbsp;</label><input class="zone"type="button" value="<"></input><input class="zone"type="button" value=">"></input>
-	</form>
-	</div>
+		<?
+		if((isset($_POST['btActionFormChoixDep']))||(isset($_POST['btActionFormChoixVisiteur'])))
+		{//début if
+		?>
+			<form name="formChoixVisiteur" method="POST">
+				<select name="lstVisiteur" class="zone">
+				<?
+				$req="select VIS_MATRICULE, VIS_NOM, VIS_PRENOM from VISITEUR where DEP_CODE='".$_SESSION['departement']."';";
+				$resultat=mysql_query($req);
+				while($maLigne=mysql_fetch_array($resultat))
+				{//début while
+					?>
+					<option value="<?=$maLigne["VIS_MATRICULE"];?>" 
+					<?
+					if($_SESSION['visiteur']==$maLigne["VIS_MATRICULE"])
+					{//début if
+						echo "selected";
+					}//fin if
+					?>><?echo $maLigne["VIS_NOM"]." ".$maLigne["VIS_PRENOM"];?></option>
+					<?
+				}//fin while
+				?>
+				</select>
+				<input type="submit" name="btActionFormChoixVisiteur" value="Séléctionner" />
+			</form>
+			<?
+		}//fin if
+		if(isset($_POST['btActionFormChoixVisiteur']))
+		{//début if
+			?>
+			<form name="formAffiVisiteur" method="POST" action="">
+				<?
+				$req="select VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, SEC_CODE from VISITEUR where VIS_MATRICULE='".$_SESSION['visiteur']."';";
+				$resultat=mysql_query($req);
+				$maLigne=mysql_fetch_array($resultat);
+				?>
+				<p>
+				NOM :<input type="text" size="7" name="VIS_NOM" class="zone" value="<?=$maLigne['VIS_NOM']?>" />
+				PRENOM :<input type="text" size="7" name="Vis_PRENOM" class="zone" value="<?=$maLigne['VIS_PRENOM']?>" />
+				</p>
+				<p>
+				ADRESSE :<input type="text" size="20" name="VIS_ADRESSE" class="zone" value="<?=$maLigne['VIS_ADRESSE']?>"/>
+				CP :<input type="text" size="5" name="VIS_CP" class="zone" value="<?=$maLigne['VIS_CP']?>"/>
+				VILLE :<input type="text" size="20" name="VIS_VILLE" class="zone" value="<?=$maLigne['VIS_VILLE']?>"/>
+				</p>
+				<p>
+				SECTEUR :<input type="text" size="1" name="SEC_CODE" class="zone" value="<?=$maLigne['VIS_SECTEUR']?>"/>
+				</p>
+				<input class="zone"type="button" value="<"></input><input class="zone"type="button" value=">"></input>
+			</form>
+		<?
+		}//fin if
+		?>
+</div>
 <?
 include("./scripts/pied.html");
 ?>
