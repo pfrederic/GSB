b01a3d045b0cf6a20ac6d84e19e445fa90ccbf5c
Pignoly Frederic <pignoly.frederic@gmail.com>
2013-08-18 18:38:45 +0200

    Ajout des premiers fichiers définitif de stat

---
+++ graphiqueCirculaireMedicamentPresente.php
@@ -0,0 +1,80 @@
+<?
+// Ce script est un test afin d'essayer de réaliser des graphiques grâce à la librairie pChart
+
+include("./scripts/parametres.php");
+include("./scripts/fonction.php");
+
+//Catégorie graphique : graphique circulaire
+
+//Appel des fichiers de la librairie nécessaire
+ include("./pChart/class/pData.class.php"); 
+ include("./pChart/class/pDraw.class.php"); 
+ include("./pChart/class/pPie.class.php"); 
+ include("./pChart/class/pImage.class.php");
+
+//Requête pour données
+$req="select MED_NOMCOMMERCIAL, count(*) nbFois from MEDICAMENT natural join PRESENTE natural join RAPPORT_VISITE group by MED_DEPOTLEGAL;";
+$resultat=mysql_query($req);
+while($ligne=mysql_fetch_array($resultat))
+{//début while
+	$nomMedoc[]=$ligne['MED_NOMCOMMERCIAL'];
+	$nbMedoc[]=$ligne['nbFois'];
+}//fin while
+
+//Création d'un objet contenant mes données pour le graphique
+ $MyData = new pData();
+ $MyData->addPoints($nbMedoc,"ScoreA");
+ $MyData->setSerieDescription("ScoreA","Application A");
+
+//Je présente la série de données à utiliser pour le graphique et je détermine le nom de l'axe verticale
+ $MyData->addPoints($nomMedoc,"Labels"); 
+ $MyData->setAbscissa("Labels"); 
+
+//Création de l'objet qui contiendra l'image
+ $myPicture = new pImage(650,300,$MyData,TRUE);
+
+//Dessine un cadre
+// $myPicture->drawRectangle(0,0,300,100,array("R"=>0,"G"=>0,"B"=>0));
+
+//Définition du fond
+ $Settings = array("R"=>173, "G"=>152, "B"=>217, "Dash"=>1, "DashR"=>193, "DashG"=>172, "DashB"=>237); 
+ $myPicture->drawFilledRectangle(0,0,750,300,$Settings); 
+
+//Fond en dégradé avec un style de révêtement
+ $Settings = array("StartR"=>209, "StartG"=>150, "StartB"=>231, "EndR"=>111, "EndG"=>3, "EndB"=>138, "Alpha"=>50); 
+ $myPicture->drawGradientArea(0,0,750,300,DIRECTION_VERTICAL,$Settings); 
+ $myPicture->drawGradientArea(0,0,750,20,DIRECTION_VERTICAL,array("StartR"=>0,"StartG"=>0,"StartB"=>0,"EndR"=>50,"EndG"=>50,"EndB"=>50,"Alpha"=>100)); 
+
+//Ajout d'une bordure pour l'image
+ $myPicture->drawRectangle(0,0,649,299,array("R"=>0,"G"=>0,"B"=>0)); 
+
+//Ecriture d'une titre de l'image 
+ $myPicture->setFontProperties(array("FontName"=>"./pChart/fonts/Silkscreen.ttf","FontSize"=>6)); 
+ $myPicture->drawText(10,13,"Medicament presente pendant les visites",array("R"=>255,"G"=>255,"B"=>255)); 
+
+//Définition de la police d'écriture par défaut
+ $myPicture->setFontProperties(array("FontName"=>"./pChart/fonts/Forgotte.ttf","FontSize"=>10,"R"=>80,"G"=>80,"B"=>80)); 
+
+ //Création de l'objet du graphique circulaire 
+ $PieChart = new pPie($myPicture,$MyData); 
+
+//Cadre avec la légendre
+ $PieChart->drawPieLegend(450,90,array("BoxSize"=>15,"FontSize"=>13));
+
+//Définition de la tranche de couleur
+ $PieChart->setSliceColor(0,array("R"=>143,"G"=>197,"B"=>0)); 
+ $PieChart->setSliceColor(1,array("R"=>97,"G"=>77,"B"=>63)); 
+ $PieChart->setSliceColor(2,array("R"=>97,"G"=>113,"B"=>63)); 
+
+//Dessin du graphique circulaire en version explosé
+ $PieChart->draw3DPie(200,165,array("Radius"=>160,"WriteValues"=>TRUE,"DataGapAngle"=>10,"DataGapRadius"=>6,"Border"=>TRUE)); 
+
+//Ecriture de la légende du graphique
+ $myPicture->setFontProperties(array("FontName"=>"./pChart/fonts/pf_arma_five.ttf","FontSize"=>6)); 
+ $myPicture->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>20)); 
+ $myPicture->drawText(325,275,"Stastistique en pourcentage des mdicament presente lors de visite",array("FontSize"=>8,"DrawBox"=>TRUE,"BoxRounded"=>TRUE,"R"=>0,"G"=>0,"B"=>0,"Align"=>TEXT_ALIGN_TOPMIDDLE));
+
+//Rendu de l'image
+  $myPicture->autoOutput("./images/testCirculaire.png");
+
+?>
---
+++ statMedicamentPresente.php
@@ -0,0 +1,13 @@
+<?
+include("./scripts/parametres.php");
+include("./scripts/entete.html");
+include("./scripts/menuGauche.html");
+?>
+<div id="contenu">
+<div width="750px" height="300px">
+<img src="./graphiqueCirculaireMedicamentPresente.php" alt>
+</div>
+</div>
+<?
+include("./scripts/pied.html");
+?>
