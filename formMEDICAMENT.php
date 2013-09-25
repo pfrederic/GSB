2c6b2acae83aa357888c59827c93e4c6e4845505
Pignoly Frederic <pignoly.frederic@gmail.com>
2013-09-25 22:42:48 +0200

    Ajout d'une recherche de médocs

--- formMEDICAMENT.php
+++ formMEDICAMENT.php
@@ -15,8 +15,11 @@ if(!isset($_SESSION['numMed']))
 
 include("./scripts/entete.html");
 include("./scripts/menuGauche.php");
+if(isset($_POST['btActionFormRechercheMedicament']))
+{//début if
+	echo "coucou";
+}
 ?>
-
 <div id="contenu">
 <?
 //Création de la requête pour récupérer le nombre de médicament à afficher
@@ -47,20 +50,28 @@ if(isset($_POST['precedent']) && $_SESSION['numMed']>=1)
 $req="Select MED_DEPOTLEGAL, MED_NOMCOMMERCIAL,FAM_CODE, MED_COMPOSITION, MED_EFFETS, MED_CONTREINDIC, MED_PRIXECHANTILLON from MEDICAMENT limit $numMed,1";
 $resultat=mysql_query($req);
 if($ligne=mysql_fetch_array($resultat))
-{
-//On récupère les informations des médicaments à afficher
-$depotLegal=$ligne['MED_DEPOTLEGAL'];
-$nomCommercial=$ligne['MED_NOMCOMMERCIAL'];
-$famille=$ligne['FAM_CODE'];
-$composition=$ligne['MED_COMPOSITION'];
-$effets=$ligne['MED_EFFETS'];
-$contrIndic=$ligne['MED_CONTREINDIC'];
-$prixEchantillon=$ligne['MED_PRIXECHANTILLON'];
-}
+{//début if
+	//On récupère les informations des médicaments à afficher
+	$depotLegal=$ligne['MED_DEPOTLEGAL'];
+	$nomCommercial=$ligne['MED_NOMCOMMERCIAL'];
+	$famille=$ligne['FAM_CODE'];
+	$composition=$ligne['MED_COMPOSITION'];
+	$effets=$ligne['MED_EFFETS'];
+	$contrIndic=$ligne['MED_CONTREINDIC'];
+	$prixEchantillon=$ligne['MED_PRIXECHANTILLON'];
+}//fin if
+
 //Puis on les affiches
 ?>
+	<h1> Pharmacopee </h1>
+	<form name="formRechercheMedicament" method="POST" action="">
+	<p>Numero dépôt légal : <input type="text" name="inputDepotLegal" /></p>
+	<p>Nom commercial : <input type="text" name="inputNomCommercial" /></p>
+	<p>Famille : <input type="text" name="inputFamille" /></p>
+	<p>Composition : <input type="text" name="inputComposition" /></p>
+	<p><input type="submit" action="btActionFormRechercheMedicament" value="Rechercher" /></p>
+	</form>
 	<form name="formMEDICAMENT" method="post" action="">
-		<h1> Pharmacopee </h1>
 		<table>
 		<tr>
 		<th>DEPOT LEGAL :</th><td><input type="text" size="10" name="MED_DEPOTLEGAL" class="zone" value="<?=$depotLegal?>" /></td>
